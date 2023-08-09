1148affb075ce8b505f49129358985058c536d40
Christopher Suarez <christophersuarez@krestomatio.com>
2023-08-09 00:00:40 -0600

    Remove duplicate Font Awesome icon options

--- src/collections/Categories.ts
+++ src/collections/Categories.ts
@@ -1,56 +1,69 @@
 import { CollectionConfig } from 'payload/types';
 import { isAdmin } from '../access';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fas, IconDefinition, IconName } from '@fortawesome/free-solid-svg-icons';
 import CategoryDescription from '../components/CategoryDescription';
 
-const iconOptions = Object.entries(fas)
-	.map(([key, obj]) => {
-		return {
-			label: obj.iconName,
-			value: key
-		};
-	});
+interface IconOption {
+	label: IconName,
+	value: string,
+}
+
+const IconOptionByIconNameMap = new Map<IconName, IconOption>();
+const allIconEntries: [string, IconDefinition][] = Object.entries(fas);
+allIconEntries.forEach(iconEntry => {
+	const iconName = iconEntry[1].iconName
+	const iconOption = IconOptionByIconNameMap.get(iconName);
+	if (!iconOption) {
+		IconOptionByIconNameMap.set(iconName, {
+			label: iconName,
+			value: iconEntry![0]
+		});
+	}
+});
+const iconOptions = Array.from(IconOptionByIconNameMap.values());
+
+// Sort iconOptions by label
+iconOptions.sort((a, b) => a.label.localeCompare(b.label));
 
 const Categories: CollectionConfig = {
-  slug: 'categories',
-  labels: {
-	singular: 'Categoría',
-	plural: 'Categorías'
-  },
-  admin: {
-    useAsTitle: 'name',
-	description: CategoryDescription,
-  },
-  access: {
-    read: () => true,
+	slug: 'categories',
+	labels: {
+		singular: 'Categoría',
+		plural: 'Categorías'
+	},
+	admin: {
+		useAsTitle: 'name',
+		description: CategoryDescription,
+	},
+	access: {
+	read: () => true,
 	create: isAdmin,
 	update: isAdmin,
 	delete: isAdmin,
-  },
-  fields: [
-    {
-		name: 'name',
-		label: 'Nombre',
-		type: 'text',
-		required: true,
-		unique: true,
 	},
-	{
-		name: 'icon',
-		label: 'Ícono',
-		type: 'select',
-		options: iconOptions,
-		required: true,
-	},
-	{
-		name: 'displayOrder',
-		label: 'Orden de visualización',
-		type: 'number',
-		required: true,
-		unique: true,
-		min: 1,
-	},
-  ],
+	fields: [
+		{
+			name: 'name',
+			label: 'Nombre',
+			type: 'text',
+			required: true,
+			unique: true,
+		},
+		{
+			name: 'icon',
+			label: 'Ícono',
+			type: 'select',
+			options: iconOptions,
+			required: true,
+		},
+		{
+			name: 'displayOrder',
+			label: 'Orden de visualización',
+			type: 'number',
+			required: true,
+			min: 1,
+		},
+	],
 };
 
 export default Categories;
