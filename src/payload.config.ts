e10e80cc9c044094699334da23cee65c9f8e005e
Christopher Suarez <christophersuarez@krestomatio.com>
2023-11-15 15:38:36 -0600

    Configure Cloud Storage with AWS S3 adapter

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,5 +1,7 @@
 import { buildConfig } from 'payload/config';
 import path from 'path';
+import { cloudStorage } from '@payloadcms/plugin-cloud-storage';
+import { s3Adapter } from '@payloadcms/plugin-cloud-storage/s3';
 import UserCollection from './collections/Users';
 import CategoryCollection from './collections/Categories';
 import ServiceCollection from './collections/Services';
@@ -34,4 +36,24 @@ export default buildConfig({
 		disable: true,
 	},
 	debug: true,
+	plugins: [
+		cloudStorage({
+			enabled: process.env.CLOUD_STORAGE_ENABLED === 'true',
+			collections: {
+				'media': {
+					adapter: s3Adapter({
+						bucket: process.env.S3_BUCKET_NAME || '',
+						config: {
+							endpoint: process.env.S3_ENDPOINT,
+							region: process.env.S3_REGION,
+							credentials: {
+								accessKeyId: process.env.S3_ACCESS_KEY_ID || '',
+								secretAccessKey: process.env.S3_SECRET_ACCESS_KEY || '',
+							},
+						},
+					})
+				},
+			},
+		}),
+	],
 });
